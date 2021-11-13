1559914ea5f7b8dac285e3657f6150c64c81b5cd
Garrett McPherson <captain_blue_of_eg@yahoo.com>
2021-11-12 19:13:41 -0800

    added more icons and made a new utility script for triggering on the month
    new script is wip, fixed the object toggle so it can local toggle. Added methods for future on off methods. Included logos for other things to link to and fixed so they had proper links.

--- Assets/USK Resources/editor/MoreTools.cs
+++ Assets/USK Resources/editor/MoreTools.cs
@@ -8,13 +8,14 @@ namespace StarterKit
 {
     public class MoreTools : EditorWindow
     {
-        private static Texture iconCyanEmu, iconCyanTriggers, iconPrefabs, iconVRWTC, iconEQS, iconWCA, iconUsharp; 
+        private static Texture iconCyanEmu, iconCyanTriggers, iconPrefabs, iconVRWTC, iconEQS, iconWCA, iconBlender, iconGimp,
+            iconKrita, iconVowgan, iconVRCDis, iconVRCVids; 
         [MenuItem("Udon Starter Kit/Suggested Resources", false, 42)]
         public static void SetWindow()
         {
             var window = (MoreTools) GetWindow(typeof(MoreTools), true, "Udon Starter Kit");
-            window.minSize = new Vector2(600, 600);
-            //window.maxSize = new Vector2(600, 600);
+            window.minSize = new Vector2(600, 500);
+            window.maxSize = new Vector2(600, 500);
             window.Show();
         
             iconCyanEmu = Resources.Load("cyanEmu-thumbnail") as Texture2D;
@@ -22,7 +23,15 @@ namespace StarterKit
             iconEQS = Resources.Load("eqs-thumbnail") as Texture2D;
             iconPrefabs = Resources.Load("prefabs-thumbnail") as Texture2D;
             iconCyanTriggers = Resources.Load("cyantrigger-thumbnail") as Texture2D;
-
+            iconWCA = Resources.Load("wca logo") as Texture2D;
+            iconVRCDis = Resources.Load("vrc discord") as Texture2D;
+            iconVRCVids = Resources.Load("vrchat tutorials") as Texture2D;
+            iconVowgan = Resources.Load("vowgan logo") as Texture2D;
+            iconBlender = Resources.Load("blender logo") as Texture2D;
+            iconKrita = Resources.Load("krita logo") as Texture2D;
+            iconGimp = Resources.Load("gimp logo") as Texture2D;
+            
+            
         }
 
         private void OnGUI()
@@ -30,12 +39,24 @@ namespace StarterKit
             if (GUILayout.Button(iconCyanEmu, GUIStyle.none)) Application.OpenURL("https://github.com/CyanLaser/CyanEmu");
             EditorGUILayout.BeginHorizontal();
             if (GUILayout.Button(iconVRWTC, GUIStyle.none)) Application.OpenURL("https://github.com/oneVR/VRWorldToolkit/releases");
-            if (GUILayout.Button(iconEQS, GUIStyle.none)) Application.OpenURL("https://github.com/oneVR/VRWorldToolkit/releases");
+            if (GUILayout.Button(iconEQS, GUIStyle.none)) Application.OpenURL("https://booth.pm/ja/items/1893576");
             EditorGUILayout.EndHorizontal();
             EditorGUILayout.BeginHorizontal();
-            if (GUILayout.Button(iconPrefabs, GUIStyle.none)) Application.OpenURL("https://github.com/oneVR/VRWorldToolkit/releases");
-            if (GUILayout.Button(iconCyanTriggers, GUIStyle.none)) Application.OpenURL("https://github.com/oneVR/VRWorldToolkit/releases");
+            if (GUILayout.Button(iconPrefabs, GUIStyle.none)) Application.OpenURL("https://docs.google.com/spreadsheets/d/e/2PACX-1vTP-eIkYLZh7pDhpO-untxy1zbuoiqdzVP2z5-vg_9ijBW7k8ZC9VP6cVL-ct5yKrySPBPJ6V2ymlWS/pubhtml");
+            if (GUILayout.Button(iconCyanTriggers, GUIStyle.none)) Application.OpenURL("https://cyanlaser.booth.pm/items/3194594");
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button(iconWCA, GUIStyle.none)) Application.OpenURL("https://github.com/Varneon/WorldCreatorAssistant");
+            if (GUILayout.Button(iconVRCDis, GUIStyle.none)) Application.OpenURL("https://discord.com/invite/vrchat");
+            if (GUILayout.Button(iconVRCVids, GUIStyle.none)) Application.OpenURL("https://www.youtube.com/watch?v=8gXzBTqlP6I");
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button(iconVowgan, GUIStyle.none)) Application.OpenURL("https://www.youtube.com/c/VowganVR");
+            if (GUILayout.Button(iconBlender, GUIStyle.none)) Application.OpenURL("https://www.blender.org/");
+            if (GUILayout.Button(iconKrita, GUIStyle.none)) Application.OpenURL("https://krita.org/en/");
+            if (GUILayout.Button(iconGimp, GUIStyle.none)) Application.OpenURL("https://www.gimp.org/");
             EditorGUILayout.EndHorizontal();
         }
     }
 }
+
---
+++ Assets/Udon Starter Kit/Utility/MonthTrigger.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace StarterKit
+{
+    public class MonthTrigger : UdonSharpBehaviour
+    {
+        [Header("Gets the month of the year as enable/triggers based on that data")]
+        [UdonSynced()] private int monthInt;
+        private DateTime masterTime;
+        void Start()
+        {
+            if (Networking.LocalPlayer.IsOwner(gameObject))
+            {
+                masterTime = DateTime.Now;
+                switch (masterTime.Month)
+                {
+                    case 1:
+                        monthInt = 1; break;
+                    case 2:
+                        monthInt = 2; break;
+                    case 3:
+                        monthInt = 3; break;
+                    case 4:
+                        monthInt = 4; break;
+                    case 5:
+                        monthInt = 5; break;
+                    case 6:
+                        monthInt = 6; break;
+                    case 7:
+                        monthInt =  7; break;
+                    case 8:
+                        monthInt = 8; break;
+                    case 9:
+                        monthInt = 9; break;
+                    case 10:
+                        monthInt = 10; break;
+                    case 11: 
+                        monthInt = 11; break;
+                    case 12:
+                        monthInt = 12; break;
+                }
+                RequestSerialization();
+                MonthSelect();
+            }
+        }
+
+        private void MonthSelect()
+        {
+            switch (monthInt)
+            {
+                case 1: break;
+                case 2: break;
+                case 3: break;
+                case 4: break;
+                case 5: break;
+                case 6: break;
+                case 7: break;
+                case 8: break;
+                case 9: break;
+                case 10: break;
+                case 11: Debug.Log("returned november"); break;
+                case 12: break;
+            }
+        }
+
+        public override void OnDeserialization()
+        {
+            MonthSelect();
+        }
+    }
+}
